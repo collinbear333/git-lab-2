581d72c8690c8e61f8d73166b753eff8d95b4a33
Collin Bartlett <cb565019@ohio.edu>
2020-09-01 18:29:49 -0400

    Last

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Collin Bartlett>
+ *        Date: <9/1/2020>
+ * Description: Lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
